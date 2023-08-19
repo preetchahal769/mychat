fd3f91e1808626feb7658340e373e2d74ad6142c
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-08-19 11:50:02 +0530

    Update dbConfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -10,7 +10,7 @@ dotenv.config();
 // MongoDB setup
 
 const dbURL= process.env.MONGO_URL; // url processing from .env
-
+  
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
