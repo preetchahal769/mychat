c14e61b0b1c419512a9241a0d2a50c164daa612f
avinasdube <avidubey712@gamil.com>
2023-08-23 12:48:39 +0530

    [new]: added new dir/components

---
+++ my-app/src/pages/Home/Home.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './Home.scss';
+
+import ichat from '../../assets/icons/mychat.png';
+
+import allroomsDark from '../../assets/icons/allroomsDark.png';
+import profile from '../../assets/images/i2.png';
+import settingsDark from '../../assets/icons/settingsDark.png';
+
+import RoomsList from '../../components/RoomsList/RoomsList';
+import ChatBox from '../../components/ChatBox/ChatBox';
+import ProfileList from '../../components/ProfileList/ProfileList';
+import SettingsList from '../../components/SettingsList/SettingsList';
+import Settings from '../Settings/Settings';
+
+const Home = () => {
+    const [listBox, setListBox] = useState('allrooms');
+
+  return (
+    <div className="homeContainer">
+        <div className="homeLeft">
+            <div className="ichatLogo">
+                <img src={ichat} alt=''></img>
+            </div>
+            <div className={`redirect ${listBox === 'allrooms' ? 'active' : 'inactive'}`}  onClick={()=>setListBox('allrooms')}>
+                <img src={allroomsDark} alt='' id='alrms'></img>
+            </div>
+            <div className="roomIcons">
+                Rooms
+            </div>
+            <div className={`redirect ${listBox === 'profile' ? 'active' : 'inactive'}`} onClick={()=>setListBox('profile')}>
+                <img src={profile} alt=''></img>
+            </div>
+            <div className={`redirect ${listBox === 'settings' ? 'active' : 'inactive'}`}  onClick={()=>setListBox('settings')}>
+                <img src={settingsDark} alt='' id='stng'></img>
+            </div>
+        </div>
+        <div className="homeMiddle">
+            <div className="listBox">
+                {
+                    listBox === 'profile' ?
+                    <ProfileList listBox='profile' /> :
+                    listBox === 'settings' ?
+                    <SettingsList listBox='settings' /> :
+                    <RoomsList listBox='rooms' />
+                }
+            </div>
+        </div>
+        <div className="homeRight">
+            {
+                
+                listBox === 'settings' ?
+                <Settings /> : 
+                <ChatBox />
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Home
