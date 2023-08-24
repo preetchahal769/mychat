5b48bb7c899e812f974d02897b2a137db018782f
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-08-24 09:36:18 +0530

    auth api added for token authorization

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,13 +1,14 @@
 // route setup for user authentication (signup and login)
 
 import express from "express";
-import { signup, login } from "../controllers/authControl.js";
+import { signup, login ,authorization } from "../controllers/authControl.js";
 
 const router = express.Router();
 
-// directing routes to {signup, login} of authControl.js
+// directing routes to {signup, login, authorization} of authControl.js
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/auth", authorization);
 
 export default router;
