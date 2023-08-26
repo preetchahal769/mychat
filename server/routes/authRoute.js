75b62185c3238c192d016a4c2ac3ca6920f6213f
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-08-26 07:15:58 +0530

    Update authRoute.js

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/authorization", verifyToken);
+router.get("/verifyToken", verifyToken);
 
 export default router;
