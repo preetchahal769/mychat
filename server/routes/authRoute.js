97dc2c638d87e1fa7132060d70e18217487861d7
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-08-30 09:04:39 +0530

    added the logut api for deleting cache form backend

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,7 +1,7 @@
 // route setup for user authentication (signup and login)
 
 import express from "express";
-import { signup, login, verifyToken } from "../controllers/authControl.js";
+import { signup, login, verifyToken, logout } from "../controllers/authControl.js";
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/verifyToken", verifyToken);
+router.get("/logout", logout);
 
 export default router;
