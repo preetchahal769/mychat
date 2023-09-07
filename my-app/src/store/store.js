73d7bec7cf2c838b5f498e4e885abbc07bcbe6c9
avinasdube <avidubey712@gamil.com>
2023-09-07 13:56:44 +0530

    [updated]: updated redux store with roomsReducer

--- my-app/src/store/store.js
+++ my-app/src/store/store.js
@@ -2,12 +2,14 @@
 
 import { configureStore } from '@reduxjs/toolkit'; // for configuring redux store 
 import settingsReducer from '../reducers/settingsReducer';
+import roomsReducer from '../reducers/roomsReducer';
 
 // creating a redux store to manage reducers
 
 const store = configureStore({
     reducer: {  // defining reducer object
         settings: settingsReducer, // creating slice named 'settings' for settingsReducer
+        rooms: roomsReducer, // creating a slice named 'rooms' for roomsReducer
     },
 });
 
