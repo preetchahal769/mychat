2a197744d0e04a89b439e7e50a53a964955849e5
avinasdube <avidubey712@gamil.com>
2023-09-07 13:53:14 +0530

    [modified]: updated rooms list ui with redux implementation

--- my-app/src/components/RoomsList/RoomsList.jsx
+++ my-app/src/components/RoomsList/RoomsList.jsx
@@ -6,66 +6,111 @@ import i2 from '../../assets/images/i2.png';
 import i3 from '../../assets/images/i3.png';
 import i4 from '../../assets/images/i4.png';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { setActiveRoomDetails } from '../../reducers/roomsReducer.js';
+
 const RoomsList = () => {
-    return (
+
+    const rooms = [
+        {
+            id: 0,
+            profileImg: i1,
+            roomName: 'My Chat Team',
+            lastMessage: 'Get done with frontend',
+        },
+        {
+            id: 1,
+            profileImg: i2,
+            roomName: 'Developers Guild',
+            lastMessage: 'Anyone here knows strapi ?',
+        },
+        {
+            id: 2,
+            profileImg: i3,
+            roomName: 'Hackers County',
+            lastMessage: 'Anonymous news ...',
+        },
+        {
+            id: 3,
+            profileImg: i4,
+            roomName: 'Indian Devs',
+            lastMessage: 'What about national data policy ?',
+        },
+    ];
+
+    // retrieving updated states of activeRoomDetails from roomsReducer
+
+    const activeRoom = useSelector((state) => state.rooms.activeRoomDetails);
+
+    const dispatch = useDispatch();
+
+    return activeRoom !== null ? (
         <div className="roomsListContainer">
             <div className="heading">Rooms</div>
+
+            {/* Search Bar */}
+
             <div className="searchBox">
                 <input type='text' placeholder='search rooms'></input>
             </div>
+
+            {/* rooms list */}
+
             <div className="roomsList">
-                <div className="roomTab">
-                    <div className="profileImg">
-                        <img src={i1} alt=''></img>
-                    </div>
-                    <div className="roomDescrip">
-                        <div className="roomName">My Chat Team</div>
-                        <div className="lastMessage">Get done with frontend</div>
-                    </div>
-                </div>
+                {rooms.map((room, index) => (
 
-                <div className="roomTab">
-                    <div className="profileImg">
-                        <img src={i2} alt=''></img>
-                    </div>
-                    <div className="roomDescrip">
-                        <div className="roomName">Developers Guild</div>
-                        <div className="lastMessage">Anyone here knows strapi ?</div>
-                    </div>
-                </div>
+                    // dispatching room details to the setActiveRoomDetails function of roomsReducer
 
-                <div className="roomTab">
-                    <div className="profileImg">
-                        <img src={i3} alt=''></img>
+                    <div className={`roomTab ${activeRoom.roomId === index ? 'active' : 'inactive'}`} key={room.id} onClick={() => dispatch(setActiveRoomDetails({
+                        roomId: room.id,
+                        roomName: room.roomName,
+                        lastMessage: room.lastMessage,
+                    }))}>
+                        
+                        <div className="profileImg">
+                            <img src={room.profileImg} alt=''></img>
+                        </div>
+                        <div className="roomDescrip">
+                            <div className="roomName">{room.roomName}</div>
+                            <div className="lastMessage">{room.lastMessage}</div>
+                        </div>
                     </div>
-                    <div className="roomDescrip">
-                        <div className="roomName">Hackers County</div>
-                        <div className="lastMessage">Anonymous news ...</div>
-                    </div>
-                </div>
-
-                <div className="roomTab">
-                    <div className="profileImg">
-                        <img src={i4} alt=''></img>
-                    </div>
-                    <div className="roomDescrip">
-                        <div className="roomName">Indian Devs</div>
-                        <div className="lastMessage">What about national data policy ?</div>
-                    </div>
-                </div>
-
-                <div className="roomTab">
-                    <div className="profileImg">
-                        <img src={i1} alt=''></img>
-                    </div>
-                    <div className="roomDescrip">
-                        <div className="roomName">React With Us</div>
-                        <div className="lastMessage">Component rendering slow any ideas ...</div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     )
+
+        :
+
+        <div className="roomsListContainer">
+            <div className="heading">Rooms</div>
+
+            {/* Search Bar */}
+
+            <div className="searchBox">
+                <input type='text' placeholder='search rooms'></input>
+            </div>
+
+            {/* rooms list */}
+
+            <div className="roomsList">
+                {rooms.map((room) => (
+                    <div className='roomTab' key={room.id} onClick={() => dispatch(setActiveRoomDetails({
+                        roomId: room.id,
+                        roomName: room.roomName,
+                        lastMessage: room.lastMessage,
+                    }))}>
+                        <div className="profileImg">
+                            <img src={room.profileImg} alt=''></img>
+                        </div>
+                        <div className="roomDescrip">
+                            <div className="roomName">{room.roomName}</div>
+                            <div className="lastMessage">{room.lastMessage}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
 }
 
 export default RoomsList
