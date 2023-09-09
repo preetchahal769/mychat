84e0c88f5d87161aaeb0c1df1c86675073aa95bb
avinasdube <avidubey712@gamil.com>
2023-09-09 13:21:08 +0530

    [new]: added profile view component

--- my-app/src/components/ProfileList/ProfileList.jsx
+++ my-app/src/components/ProfileList/ProfileList.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './ProfileList.scss';
 
+import avinas from '../../assets/images/i1.png'
+
 const ProfileList = () => {
   return (
-    <div>ProfileList</div>
+    <div className="profileContainer">
+      <div className="heading">Profile</div>
+      <div className="profileBox">
+        <div className="profileImageBox">
+          <img src={avinas} alt=''></img>
+        </div>
+        <div className="profileDetailsBox">
+          <button><label>Name</label>Avinash Dubey</button>
+          <button><label>Username</label>avinasdube</button>
+          <button><label>Email</label>avinasdube@gmail.com</button>
+          <button><label>Phone</label>9999999999</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
