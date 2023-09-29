61c88cf3470e2560eaca9c84e2806889dfdc0964
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-09-29 08:50:03 +0530

    Create roomRoute.js

---
+++ server/routes/roomRoute.js
@@ -0,0 +1,15 @@
+// route setup for user authentication (signup and login)
+
+import express from "express";
+import { createRoom , removeRoom , userList } from "../controllers/roomControl.js";
+
+const router = express.Router();
+
+// directing routes to {create, delet, userlist} of roomControl.js
+
+router.post("/newroom", createRoom);
+router.post("/deletroom", removeRoom);
+router.get("/userlist", userList);
+
+
+export default router;
